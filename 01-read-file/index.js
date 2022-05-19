d30ae7c22d14fe22d4587d2429beedc964dca254
AnatoliyIliev <vekok12@gmail.com>
2022-05-19 21:52:42 +0300

    feat: finish 02-write-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,21 +3,8 @@ const path = require('path');
 
 const text = path.join(__dirname, 'text.txt');
 
-const stream = new fs.ReadStream(text, {encoding: 'utf-8'});
+const stream = fs.createReadStream(text, 'utf-8');
 
-stream.on('readable', () => {
-  const data = stream.read();
+stream.on('data', data => {
   console.log(data);
-});
-
-stream.on('end', () => {
-  console.log('end');
-});
-
-stream.on('error', (error) => {
-  if(error.code === 'ENOENT') {
-    console.log('File no found');
-  } else {
-    console.log(error);
-  }
 });
---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = require('process');
+
+const text = path.join(__dirname, 'textFile.txt');
+const stream = fs.createWriteStream(text, 'utf-8');
+
+stdout.write(
+  'Hello! Write what you want and press "Strl - С" or "exit" to exit. Thank you.\n',
+);
+
+stdin.on('data', data => {
+  if (data.toString().trim() === 'exit') {
+    process.exit();
+  } else {
+    stream.write(data.toString());
+  }
+});
+
+process.on('SIGINT', () => process.exit());
+process.on('exit', () => {
+  console.log('Good luck');
+});
