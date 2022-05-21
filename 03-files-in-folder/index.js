a214e0c62b19dc52ae56d2f486e8779cd14c26e0
AnatoliyIliev <vekok12@gmail.com>
2022-05-21 19:19:47 +0300

    feat: finish 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const { readdir } = require('fs/promises');
+const { stat } = require('fs');
+
+const pathFolder = path.join(__dirname, 'secret-folder');
+const readFolder = async folder => {
+  try {
+    const readDir = await readdir(folder, { withFileTypes: true });
+    for (const file of readDir) {
+      const pathFile = `${folder}/${file.name}`;
+      if (file.isFile()) {
+        stat(pathFile, (err, stats) => {
+          if (err) console.log(err);
+          else {
+            const parseFile = path.parse(pathFile);
+            console.log(
+              `${parseFile.name} - ${parseFile.ext.slice(1)} - ${stats.size}b`,
+            );
+          }
+        });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+readFolder(pathFolder);
+
+// stat(file,  (err, stats) => {
+
+//     if (err) console.log(err);
+//     else console.log(stats);
+//   });
